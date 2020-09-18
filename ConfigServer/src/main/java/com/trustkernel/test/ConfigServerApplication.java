b358dc9cf14443f6aeda8105a11b26a82fd9f3c8
qdl <qindanlei@trustkernel.com>
2020-09-18 15:27:35 +0800

    add new config data

--- ConfigServer/src/main/java/com/trustkernel/test/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/trustkernel/test/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.trustkernel.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 /**
  * 这里不能使用@SpringCloudApplication注解，会报错
@@ -9,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @author qdl
  *
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigServer   //开启config 的server功能
 public class ConfigServerApplication {
